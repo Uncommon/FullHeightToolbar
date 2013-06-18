d198dd2d5ce31586b572e5b6c1a485fcc334591f
David Catmull <catmull@google.com>
2013-06-18 16:59:38 -0600

    Hide undesirable menu items

--- Library/UUFullHeightToolbarDelegate.m
+++ Library/UUFullHeightToolbarDelegate.m
@@ -4,6 +4,57 @@
 
 @implementation UUFullHeightToolbarDelegate
 
+- (id)init
+{
+  if ((self = [super init]) == nil)
+    return nil;
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(beginMenuTracking:)
+             name:NSMenuDidBeginTrackingNotification
+           object:nil];
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(endMenuTracking:)
+             name:NSMenuDidEndTrackingNotification
+           object:nil];
+  return self;
+}
+
+- (void)updateContextMenu:(NSMenu *)menu hide:(BOOL)hide
+{
+  // Hide or show items that change the display mode or icons size, and any
+  // separators that become redundant.
+  NSMenu *mainMenu = [NSApp mainMenu];
+
+  if ((menu == mainMenu) || ([menu supermenu] == mainMenu))
+    return;
+
+  BOOL changedPrevious = NO;
+
+  for (NSMenuItem *item in menu.itemArray) {
+    if (item.isSeparatorItem && changedPrevious)
+      [item setHidden:hide];
+    if ((item.action == @selector(changeToolbarDisplayMode:)) ||
+        (item.action == @selector(toggleUsingSmallToolbarIcons:))) {
+      [item setHidden:hide];
+      changedPrevious = YES;
+    } else {
+      changedPrevious = NO;
+    }
+  }
+}
+
+- (void)beginMenuTracking:(NSNotification *)note
+{
+  [self updateContextMenu:[note object] hide:YES];
+}
+
+- (void)endMenuTracking:(NSNotification *)note
+{
+  [self updateContextMenu:[note object] hide:NO];
+}
+
 - (void)toolbarWillAddItem:(NSNotification *)notification
 {
   NSToolbarItem *item = (NSToolbarItem *)[notification userInfo][@"item"];
