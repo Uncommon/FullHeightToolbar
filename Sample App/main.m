68d239328022a3e627b54ca7c9b56820ab48449d
David Catmull <catmull@google.com>
2013-06-19 12:01:14 -0600

    Big rename: support files

--- Sample App/main.m
+++ Sample App/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  UCFullHeightToolbar
-//
-//  Created by David Catmull on 6/6/13.
-//  Copyright (c) 2013 David Catmull. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
